7e7658849e105e060945255219f39a166deabc35
HM Jawas <hmjawad087@gmail.com>
2022-04-19 07:03:04 +0500

    Javascript

---
+++ Day 1_Expanding Cards/script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    remove();
+    panel.classList.add('active');
+  });
+});
+
+function remove() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
